fe8f7e35937c4d6ba440f159a63c414627b719a8
Stephen0111 <86751563+Stephen0111@users.noreply.github.com>
2025-06-21 23:39:02 +0100

    lg1

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -154,7 +154,7 @@ const Login = () => {
             className="auth-side-panel text-white d-none d-md-flex flex-column justify-content-center align-items-center"
           >
             <img
-              src="/client/ZINGA.png"
+              src="/ZINGA.png"
               alt="Logo"
               className="mb-4 logo"
               style={{ width: "120px", height: "auto" }}
