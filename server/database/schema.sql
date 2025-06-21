5e366d98f758f16b41455c7d415c80077bea066b
Stephen0111 <86751563+Stephen0111@users.noreply.github.com>
2025-06-21 23:50:29 +0100

    tes2t

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -7,3 +7,14 @@ CREATE TABLE users (
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
