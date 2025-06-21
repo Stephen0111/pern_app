69bc61011bf89399edf0553c4fd70e85ab4e5088
Stephen0111 <86751563+Stephen0111@users.noreply.github.com>
2025-06-22 00:06:02 +0100

    1tes32t

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,13 +1,15 @@
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
- firstname VARCHAR(255)  NOT NULL,
- Surname VARCHAR(255)  NOT NULL,
+  firstname VARCHAR(255)  NOT NULL,
+  surname VARCHAR(255)  NOT NULL,
   phoneNumber VARCHAR(255)  NOT NULL,
   address VARCHAR(255)  NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
+-- Renamed the session table to "user_sessions" to match server.js configuration
 CREATE TABLE "user_sessions" (
   "sid" varchar NOT NULL COLLATE "default",
   "sess" json NOT NULL,
@@ -15,6 +17,6 @@ CREATE TABLE "user_sessions" (
 )
 WITH (OIDS=FALSE);
 
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+ALTER TABLE "user_sessions" ADD CONSTRAINT "user_sessions_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
-CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+CREATE INDEX "IDX_user_sessions_expire" ON "user_sessions" ("expire");
